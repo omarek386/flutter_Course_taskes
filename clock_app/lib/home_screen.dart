c33da4cd19dea954199afb5d4ff88e60365d1192
Omar Khalifa <omarek386@gmail.com>
2024-12-22 01:48:34 +0200

    Implement customizable clock widget with color and size options in HomeScreen

--- clock_app/lib/home_screen.dart
+++ clock_app/lib/home_screen.dart
@@ -1,6 +1,7 @@
+import 'dart:developer';
+import 'package:clock_app/clock_widget.dart';
+import 'package:clock_app/device_size.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:one_clock/one_clock.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,29 +11,74 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late double clockSize = context.width * 0.5;
+  bool isAnalog = true;
+  Color clockColor = Colors.black;
+  Color hourMinuetsColor = Colors.black;
+  Color secondsColor = Colors.black;
+  Color numberColor = Colors.black;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
+        child: Column(
           children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
+            SizedBox(
+              width: double.infinity,
+              child: const DrawerHeader(
+                // curve: Curves.easeInOutCirc,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Clock App'),
               ),
-              child: Text('Drawer Header'),
             ),
             ListTile(
-              title: const Text('Item 1'),
+              title: const Text('Analog Clock'),
               onTap: () {
+                setState(() {
+                  isAnalog = true;
+                });
                 Navigator.pop(context);
               },
             ),
             ListTile(
-              title: const Text('Item 2'),
+              title: const Text('Digital Clock'),
               onTap: () {
+                setState(() {
+                  isAnalog = false;
+                });
                 Navigator.pop(context);
+              },
+            ),
+            pickColor(method: selectColorOfClock(), title: 'Clock Color'),
+            isAnalog
+                ? pickColor(
+                    method: selectColorOfHourMinuets(),
+                    title: 'Hour and Minute Color')
+                : Container(),
+            isAnalog
+                ? pickColor(
+                    method: selectColorOfSeconds(), title: 'Seconds Color')
+                : Container(),
+            isAnalog
+                ? pickColor(
+                    method: selectColorOfNumbers(), title: 'Numbers Color')
+                : Container(),
+            Slider(
+              min: context.width * 0.3,
+              max: context.width * 0.9,
+              value: clockSize,
+              activeColor: Colors.blue,
+              label: 'Clock Size',
+              onChanged: (value) {
+                setState(
+                  () {
+                    clockSize = value;
+                    log('Clock size: $value');
+                  },
+                );
               },
             ),
           ],
@@ -41,40 +87,114 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text('Clock App'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          AnalogClock(
-            decoration: BoxDecoration(
-                border: Border.all(width: 2.0, color: Colors.black),
-                color: Colors.transparent,
-                shape: BoxShape.circle),
-            width: 150.0,
-            height: 150,
-            isLive: true,
-            hourHandColor: Colors.black,
-            minuteHandColor: Colors.black,
-            showSecondHand: true,
-            numberColor: Colors.black87,
-            showNumbers: true,
-            showAllNumbers: true,
-            textScaleFactor: 1.4,
-            showTicks: true,
-            showDigitalClock: true,
-            datetime: DateTime.now(),
-          ),
-          DigitalClock(
-              showSeconds: false,
-              isLive: true,
-              digitalClockTextColor: Colors.black,
-              format: DateFormat.jms().pattern,
-              decoration: const BoxDecoration(
-                  color: Colors.yellow,
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.all(Radius.circular(15))),
-              datetime: DateTime.now())
-        ],
+      body: Center(
+        child: ClockWidget(
+            clockSize: clockSize,
+            isAnalog: isAnalog,
+            clockColor: clockColor,
+            hourMinuteColor: hourMinuetsColor,
+            numberColor: numberColor,
+            secondHandColor: secondsColor),
+      ),
+    );
+  }
+
+  Widget pickColor({required Widget method, required String title}) {
+    return Column(
+      children: [
+        Divider(),
+        Text(title),
+        SizedBox(height: 50, child: method),
+      ],
+    );
+  }
+
+  Widget selectColorOfClock() {
+    return ListView.builder(
+        itemCount: Colors.primaries.length,
+        scrollDirection: Axis.horizontal,
+        // padding: EdgeInsets.only(left: 10),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: colorBox(
+                color: Colors.primaries[index],
+                onTap: () {
+                  setState(() {
+                    clockColor = Colors.primaries[index];
+                  });
+                }),
+          );
+        });
+  }
+
+  Widget selectColorOfHourMinuets() {
+    return ListView.builder(
+        itemCount: Colors.primaries.length,
+        scrollDirection: Axis.horizontal,
+        // padding: EdgeInsets.only(left: 10),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: colorBox(
+                color: Colors.primaries[index],
+                onTap: () {
+                  setState(() {
+                    hourMinuetsColor = Colors.primaries[index];
+                  });
+                }),
+          );
+        });
+  }
+
+  Widget selectColorOfSeconds() {
+    return ListView.builder(
+        itemCount: Colors.primaries.length,
+        scrollDirection: Axis.horizontal,
+        // padding: EdgeInsets.only(left: 10),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: colorBox(
+                color: Colors.primaries[index],
+                onTap: () {
+                  setState(() {
+                    secondsColor = Colors.primaries[index];
+                  });
+                }),
+          );
+        });
+  }
+
+  Widget selectColorOfNumbers() {
+    return ListView.builder(
+        itemCount: Colors.primaries.length,
+        scrollDirection: Axis.horizontal,
+        // padding: EdgeInsets.only(left: 10),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: colorBox(
+                color: Colors.primaries[index],
+                onTap: () {
+                  setState(() {
+                    numberColor = Colors.primaries[index];
+                  });
+                }),
+          );
+        });
+  }
+
+  Widget colorBox({required Color color, required Function() onTap}) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 50,
+        width: 50,
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+        ),
       ),
     );
   }
